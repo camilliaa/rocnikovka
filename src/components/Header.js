6360e8563da8a22e3e7bc106c883f028a1e8259c
JanStork <jan.stork@protonmail.com>
2024-11-03 11:17:59 +0100

    logo, title, ico

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import '../styles/Header.css'; 
+import logo from '../img/logoW.png';
 
 function Header() {
   return (
-    <header className="headerContainer">
-      <nav>
-        <div class="footer-left">MojeLogo</div>
-        <div class="footer-right">
-            <ul>
-                <li><a href="#about">O mně</a></li>
-                <li><a href="#gallery">Galerie</a></li>
-                <li><a href="#booking">Booking</a></li>
-                <li><a href="#contact">Kontakt</a></li>
-                <li><a href="#login">Přihlášení</a></li>
-            </ul>
-        </div>
-      </nav>
-    </header>
+    <nav>
+      <div class="logo"><img src={logo} alt="Logo" /></div>
+      <div>
+          <ul>
+              <li><a href="#about">O mně</a></li>
+              <li><a href="#gallery">Galerie</a></li>
+              <li><a href="#booking">Booking</a></li>
+              <li><a href="#contact">Kontakt</a></li>
+              <li><a href="#login">Přihlášení</a></li>
+          </ul>
+      </div>
+    </nav>
   );
 }
 
