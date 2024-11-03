6e7aba91fc34289e6aef0ec0ea97bee94cd2b6d6
JanStork <jan.stork@protonmail.com>
2024-11-03 18:35:29 +0100

    fixnutej button

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -7,16 +7,16 @@ function MainContent() {
     return (
         <main>
             <div>
-                <img className='bg' src={BGImg} alt='backup-background'/>
+                <img className='bg' src={BGImg} alt='backup-background' z-index="-2"/>
                 <video className='video' autoPlay loop muted playsInline preload='auto'>
                     <source src={BGVid} type='video/mp4' />
                 </video>
             </div>
             <div className="introduction">
                 <h1>Dan Di Rezo Daněk</h1>
-                <p>tatér od roku 2013</p>
-                <p>tetování Znojmo & Brno</p>
-                <a href="#booking"><button>Booking</button></a>
+                <a>tatér od roku 2013</a>
+                <a>tetování Znojmo & Brno</a>
+                <button className="booking-button" onClick={() => window.location.href = "#booking"}>Booking</button>
             </div>
         </main>
     );
