2037d95c543066409df0dd5ea3626556528ab760
JanStork <jan.stork@protonmail.com>
2024-11-28 19:19:41 +0100

    udělal jsem cssko na booking aby se mi víc líbilo

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -3,34 +3,32 @@ import '../styles/Booking.css';
 
 function Booking() {
     return (
-        <div className="container">
+        <div className="booking">
             <h1>BOOKING</h1>
 
             <div className="form-group">
                 <label htmlFor="name">Jméno *</label>
-                <input type="text" id="name" placeholder="Napiš jméno" />
+                <input type="text" id="name" placeholder="Vaše jméno" />
             </div>
-
             <div className="form-group">
                 <label htmlFor="email">E-mail *</label>
-                <input type="email" id="email" placeholder="Napiš e-mail" />
+                <input type="email" id="email" placeholder="Váš e-mail" />
             </div>
-
             <div className="form-group">
                 <label htmlFor="phone">Telefon *</label>
-                <input type="tel" id="phone" placeholder="Napiš telefon" />
+                <input type="tel" id="phone" placeholder="Vaše telefonní číslo" />
             </div>
-
             <div className="form-group">
                 <label htmlFor="placement">Umístění tetování *</label>
-                <input type="text" id="placement" placeholder="Např. předloktí" />
+                <input type="text" id="placement" placeholder="stehno, předloktí, kotník..." />
             </div>
 
             <div className="form-group">
-                <label htmlFor="placement-photo">Fotka umístění</label>
-                <div className="file-input">
-                    <input type="file" id="placement-photo" />
-                    <span className="file-description">Napiš fotka vašeho předloktí (max. 5MB)</span>
+                <label htmlFor="placement-photo">Fotografie umístění</label>
+                    <div className="file-input">
+                    <label htmlFor="file-upload" className="custom-button">Vyberte soubor</label>
+                    <input type="file" id="placement-photo" className="hidden-file" />
+                    <span className="file-description">(max. 5MB)</span>
                 </div>
             </div>
 
@@ -42,24 +40,24 @@ function Booking() {
 
             <div className="reference-photos">
                 <div className="file-input">
-                    <label>Referenční fotka motivu</label>
-                    <input type="file" />
-                    <span className="file-description">Není zvolený žádný soubor<br />(max. 3MB)</span>
+                    <label htmlFor="file-upload" className="custom-button">Vyberte soubor</label>
+                    <input type="file" id="file-upload" className="hidden-file" />
+                    <span className="file-description">Není zvolen žádný soubor<br />(max. 3MB)</span>
                 </div>
                 <div className="file-input">
-                    <label>Referenční fotka motivu</label>
-                    <input type="file" />
-                    <span className="file-description">Není zvolený žádný soubor<br />(max. 3MB)</span>
+                    <label htmlFor="file-upload" className="custom-button">Vyberte soubor</label>
+                    <input type="file" id="file-upload" className="hidden-file" />
+                    <span className="file-description">Není zvolen žádný soubor<br />(max. 3MB)</span>
                 </div>
                 <div className="file-input">
-                    <label>Referenční fotka motivu</label>
-                    <input type="file" />
-                    <span className="file-description">Není zvolený žádný soubor<br />(max. 3MB)</span>
+                    <label htmlFor="file-upload" className="custom-button">Vyberte soubor</label>
+                    <input type="file" id="file-upload" className="hidden-file" />
+                    <span className="file-description">Není zvolen žádný soubor<br />(max. 3MB)</span>
                 </div>
                 <div className="file-input">
-                    <label>Referenční fotka motivu</label>
-                    <input type="file" />
-                    <span className="file-description">Není zvolený žádný soubor<br />(max. 3MB)</span>
+                    <label htmlFor="file-upload" className="custom-button">Vyberte soubor</label>
+                    <input type="file" id="file-upload" className="hidden-file" />
+                    <span className="file-description">Není zvolen žádný soubor<br />(max. 3MB)</span>
                 </div>
             </div>
 
